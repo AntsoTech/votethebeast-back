5d50c24904689abf692f7a851ce234e1faa9cd2b
AntsoTech <antso-rakotoson@hotmail.fr>
2022-07-21 20:34:47 +0200

    ok

--- interfaces/ICountry.ts
+++ interfaces/ICountry.ts
@@ -3,5 +3,5 @@ import { RowDataPacket } from 'mysql2';
 export default interface ICountry extends RowDataPacket {
   id: number;
   name: string;
-  flag:string;
+  flag: string;
 }
--- models/countrymodel.ts
+++ models/countrymodel.ts
@@ -2,11 +2,10 @@ import connection from '../db-config';
 import ICountry from '../interfaces/ICountry';
 
 const getAllCountries = async (): Promise<ICountry[]> => {
-    const results = await connection
-      .promise()
-      .query<ICountry[]>(
-        'SELECT * FROM countries');
-    return results[0];
-  };
+  const results = await connection
+    .promise()
+    .query<ICountry[]>('SELECT * FROM countries');
+  return results[0];
+};
 
-  export default {getAllCountries};
+export default { getAllCountries };
